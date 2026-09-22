39ff1fcdf07f7c32d4ba8dc69737f8695420d618
mariana-sesc <mariana.arruda.valerio2@gmail.com>
2026-09-22 14:18:46 -0400

    add if else 2

---
+++ if-else-2.js
@@ -0,0 +1,7 @@
+let diaDasemana = "quarta-feira";
+
+if (diaDasemana === "sábado" || diaDaSemana === "domingo") {
+    conslole.log("É fim de semana!");
+} else {
+    console.log("É dia de semana.");
+}
